4e18f4865a96456f23b76a032454baf8e3280cc5
gianmartinez32 <gian.martinez@devitech.com.co>
2023-01-17 08:31:15 -0500

    cambiar orden peticion

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -4,10 +4,10 @@ const dashboard_router = Router();
 
 dashboard_router.get("/dashboard/indicators",getIndicatorsAll)
 dashboard_router.get("/dashboard/indicators/:id_store",getIndicatorsStore)
-dashboard_router.get("/dashboard/charts/incomes/:id_store",getIncomeStoreChartDataWeek)
 dashboard_router.get("/dashboard/charts/incomes/",getIncomeChartDataWeek)
-dashboard_router.get("/dashboard/charts/costs/:id_store",getCostsStoreChartDataWeek)
+dashboard_router.get("/dashboard/charts/incomes/:id_store",getIncomeStoreChartDataWeek)
 dashboard_router.get("/dashboard/charts/costs/",getCostsChartDataWeek)
+dashboard_router.get("/dashboard/charts/costs/:id_store",getCostsStoreChartDataWeek)
 
 
 
