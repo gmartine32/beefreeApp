955d01c1e84f1e1de9252f1853d9a4765ab751fb
gianmartinez32 <gian.martinez@devitech.com.co>
2023-03-11 09:34:48 -0500

    fix: add dates

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import './stores/store.js'
 import './movements/movement.js'
 import './orders/order.js'
 import './stats/stats.js'
+import dayjs from "dayjs";
+import moment from "moment";
 
 
 
@@ -22,6 +24,13 @@ const main = async () => {
     app.listen(process.env.PORT, ()=> {
       console.log("Contectado a productivo xd",process.env.PORT);
     })
+
+    setInterval(() => {
+      console.log('dayjs',dayjs().format('YYYY-MM-DD HH:mm:ss'))
+      console.log('moment',moment().format('YYYY-MM-DD HH:mm:ss'))
+      console.log('date',new Date())
+
+    }, 4000);
     
   } catch (error) {
     console.log('ERRROR:',error);
