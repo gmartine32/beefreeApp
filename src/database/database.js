29938093fc73a144b7695a3aaac30921319e5d15
gianmartinez32 <gian.martinez@devitech.com.co>
2023-03-17 14:16:28 -0500

    se cambio la conexion de la base de datos

--- src/database/database.js
+++ src/database/database.js
@@ -1,10 +1,12 @@
 import Sequelize from "sequelize";
 
-/* export const sequelize = new Sequelize("beefreedb", "postgres", "123456", {
-  dialect: "postgres",
-  host: "localhost",
-}); */
+// export const sequelize = new Sequelize("beefreedb", "admin", "123456", {
+//   dialect: "postgres",
+//   host: "localhost",
+// });
 
 
+const databaseUrl = 'postgres://admin:123456@31.220.56.22:5432/beefreedb';
+const db = new Sequelize(databaseUrl);
 
-export const sequelize = new Sequelize('postgres://admin:uaOYWIDnlylRN3Yx1xbxPWsZWMFtg5Yf@dpg-cf2al094reb5o452enpg-a.oregon-postgres.render.com/beefreedb?ssl=true',{keepAliveTimeout:true,logging: true, timezone: 'America/Bogota',})
+//export const sequelize = new Sequelize('postgres://admin:uaOYWIDnlylRN3Yx1xbxPWsZWMFtg5Yf@dpg-cf2al094reb5o452enpg-a.oregon-postgres.render.com/beefreedb?ssl=true',{keepAliveTimeout:true,logging: true, timezone: 'America/Bogota',})
