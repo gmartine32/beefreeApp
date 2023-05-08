56eef2495e27202de612a1b08990a5ce2bacccfb
gianmartinez32 <gian.martinez@devitech.com.co>
2023-05-08 17:20:40 -0500

    fix

--- src/stats/stat.routes.js
+++ src/stats/stat.routes.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import { createStat, deleteStat, getStatByStoreFilter, getStatsByStoreCustomDate } from "./stats.controller.js";
-import { createMultipleStats } from "./stats.services.js";
+import { createMultiStat, createStat, deleteStat, getStatByStoreFilter, getStatsByStoreCustomDate } from "./stats.controller.js";
 
 const stat_router = Router();
 
 stat_router.post('/',createStat)
-stat_router.post('/createMulti',createMultipleStats)
+stat_router.post('/createMulti',createMultiStat)
 stat_router.post('/store/:id',getStatsByStoreCustomDate)
 stat_router.post('/store/:id/:filter',getStatByStoreFilter)
 stat_router.delete('/:id',deleteStat)
