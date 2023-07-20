288d422e856d3a35793bac4cfbc2053b0f029edd
Gian Franco Martínez Villa <gian.martinez@devitech.com.co>
2023-07-20 12:42:51 -0500

    fix: se añadio timezone

--- src/database/database.js
+++ src/database/database.js
@@ -7,6 +7,6 @@ import Sequelize from "sequelize";
 
 
 const databaseUrl = 'postgres://admin:123456@localhost:5432/beefreedb';
-export const sequelize = new Sequelize(databaseUrl,{keepAliveTimeout:true,logging: true});
+export const sequelize = new Sequelize(databaseUrl,{keepAliveTimeout:true,logging: true,timezone: 'America/Bogota'});
 
 // export const sequelize = new Sequelize('postgres://admin:uaOYWIDnlylRN3Yx1xbxPWsZWMFtg5Yf@dpg-cf2al094reb5o452enpg-a.oregon-postgres.render.com/beefreedb?ssl=true',{keepAliveTimeout:true,logging: true, timezone: 'America/Bogota',})
