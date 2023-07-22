185c96380fdc893697f605610d02a2db52946197
root <root@vps.server.local>
2023-07-22 17:49:58 +0000

    se agregaron 2 endpoints nuevos

--- src/orders/order.router.js
+++ src/orders/order.router.js
@@ -1,19 +1,22 @@
 import { Router } from "express";
-import { createOrder, deleteOrder, getDataOrderByCityFilter, getDataOrderByCityRangeDate, getDataStateByCityFilter, getOrderByStore, getOrderByStoreCustomDate, getOrderByStoreFilter, getOrders, getOrderStateByStore } from "./order.controller.js";
+import { createOrder, deleteOrder, getDataOrderByCityFilter, getDataOrderByCityRangeDate, getDataStateByCityFilter, getOrderByStore, getOrderByStoreCustomDate, getOrderByStoreFilter, getOrderConsolidatedByStore, getOrderConsolidatedByStoreFilter, getOrderConsolidatedCountryByStore, getOrderConsolidatedCountryByStoreFilter, getOrders, getOrderStateByStore } from "./order.controller.js";
 
 const order_router = Router();
 
-order_router.post('/order',createOrder)
-order_router.get('/order/store/',getOrders)
-order_router.get('/order/store/:id',getOrderByStore)
-order_router.post('/order/store/:id',getOrderByStoreCustomDate)
-order_router.get('/order/store/:id/:filter',getOrderByStoreFilter)
-order_router.delete('/order/:id',deleteOrder)
-order_router.post('/order/store/cities/:id',getDataOrderByCityRangeDate)
-order_router.get('/order/store/cities/:id/:filter',getDataOrderByCityFilter)
-order_router.post('/order/store/states/:id',getOrderStateByStore)
-order_router.post('/order/consolidated/',getOrderStateByStore)
-order_router.get('/order/store/states/:id/:filter',getDataStateByCityFilter)
+order_router.post('/order', createOrder)
+order_router.get('/order/store/', getOrders)
+order_router.get('/order/store/:id', getOrderByStore)
+order_router.post('/order/store/:id', getOrderByStoreCustomDate)
+order_router.get('/order/store/:id/:filter', getOrderByStoreFilter)
+order_router.delete('/order/:id', deleteOrder)
+order_router.post('/order/store/cities/:id', getDataOrderByCityRangeDate)
+order_router.get('/order/store/cities/:id/:filter', getDataOrderByCityFilter)
+order_router.post('/order/store/states/:id', getOrderStateByStore)
+order_router.post('/order/consolidated', getOrderConsolidatedByStore)
+order_router.post('/order/consolidated/filter', getOrderConsolidatedByStoreFilter)
+order_router.post('/order/consolidated/country', getOrderConsolidatedCountryByStore)
+order_router.post('/order/consolidated/country/filter', getOrderConsolidatedCountryByStoreFilter)
+order_router.get('/order/store/states/:id/:filter', getDataStateByCityFilter)
 
 
 
